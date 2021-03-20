d339c9aaf77a74643370363b478948c794897253
Simon Gábor <simon.gabor@aut.bme.hu>
2021-03-20 15:29:46 +0100

    ef code

--- manuscript/code/efcore-order.cs
+++ manuscript/code/efcore-order.cs
@@ -3,6 +3,6 @@ public class Order
     public int Id { get; set; }
     public DateTime OrderDate { get; set; }
 
-    public ICollection<ProductOrder> ProductOrders { get; } 
-                                        = new List<ProductOrder>();
+    public ICollection<OrderItem> OrderItems { get; } 
+                                        = new List<OrderItem>();
 }
--- manuscript/code/efcore-orderitem.cs
+++ manuscript/code/efcore-orderitem.cs
@@ -1,6 +1,8 @@
-public class ProductOrder
+public class OrderItem
 {
     public int Id { get; set; }
+
+    public int Quantity { get; set; }
 
     public int ProductId { get; set; }
     public Product Product { get; set; }
